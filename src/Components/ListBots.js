43d5a4a4d1b0259b04d07831716ea214dd3d26e3
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-01-23 02:02:44 +0300

    Completed

--- src/Components/ListBots.js
+++ src/Components/ListBots.js
@@ -1,29 +1,37 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeartPulse, faShieldHalved, faBolt } from '@fortawesome/free-solid-svg-icons'
-
+import { faHeartPulse, faShieldHalved, faBolt, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+// import BotsDetails from './Components/BotsDetails'
 const Card = ({ item, handleOnClick }) => {
+    console.log(item.bot_class);
+
+    function deletePost() {
+        // hOMEWORK
+        fetch(`http://localhost:8001/bots/${item.id}`, {
+            method: "DELETE"
+        })
+
+
+    }
+
 
     return (
         <div onClick={() => handleOnClick(item.id)} className="card col-2 p-0 m-2">
+            <button onClick={deletePost} className="btn btn-sm btn-block btn-danger"> <FontAwesomeIcon icon={faCircleXmark} /></button>
             <img src={item.avatar_url} className="card-img-top" alt="..." />
             <div className="card-deck">
-                <h5 className="card-title ">{item.name} <FontAwesomeIcon icon={() => {
-                    if (item.bot_class === "Support") {
-                        return faHeartPulse
-                    } else if (item.bot_class === "Medics") {
-                        return faHeartPulse
-                    }
-                }} /></h5>
+                <h5 className="card-title ">{item.name} <FontAwesomeIcon icon={item.bot_class} /></h5>
                 <p className="card-text">{item.catchphrase}</p>
-                {/* <a href={`/products/${item.id}`} className="btn btn-sm text-small btn-primary btn-block m-2">View Item Details</a>
-                <button className="btn btn-sm text-small btn-danger btn-block">Delete Items</button> */}
+                <a href={`/bots/${item.id}`} className="btn btn-sm text-small btn-primary btn-block m-2">View Item Details</a>
+                <button className="btn btn-sm text-small btn-danger btn-block">Delete Items</button>
 
                 <div class="card-footer mx-12">
-                    <small class="text-muted"> <FontAwesomeIcon icon={faHeartPulse} />{item.health}</small>
-                    <small class="text-muted"> <FontAwesomeIcon icon={faBolt} />{item.damage}</small>
-                    <small class="text-muted"> <FontAwesomeIcon icon={faShieldHalved} />{item.armor}</small>
+                    <small className="text-muted"> <FontAwesomeIcon icon={faHeartPulse} />{item.health}</small>
+                    <small className="text-muted"> <FontAwesomeIcon icon={faBolt} />{item.damage}</small>
+                    <small className="text-muted"> <FontAwesomeIcon icon={faShieldHalved} />{item.armor}</small>
 
                 </div>
+
+
 
             </div>
         </div>
